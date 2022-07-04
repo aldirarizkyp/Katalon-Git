8e5ad7ccefa3a848d60c5ce5bc3b3231c1ef3911
Aldiransyah Rizky Putra <87604327+aldirarizkyp@users.noreply.github.com>
2022-07-04 11:17:44 +0700

    initial commit

---
+++ Test Suites/TS Login/TS Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test suite tentang positif dan negatif case dari login</description>
+   <name>TS Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d916a596-25d1-44c5-93cb-1230fd40ad88</testSuiteGuid>
+</TestSuiteEntity>
